afb57e4a0e55c86bdbed7913295d94dd54c15a13
Philip Meshach <Philipmeshach.pm@gmail.com>
2022-12-29 16:20:02 +0530

    created the Repository class and write a method inside it

--- src/main/java/com/niit/Customer/domain/Customer.java
+++ src/main/java/com/niit/Customer/domain/Customer.java
@@ -5,10 +5,12 @@
  */
 package com.niit.Customer.domain;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 public class Customer {
+    @Id
     private int customerId;
     private String customerName;
     private long customerPhoneNo;
---
+++ src/main/java/com/niit/Customer/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.niit.Customer.repository;
+
+import com.niit.Customer.domain.Customer;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+
+public interface CustomerRepository extends MongoRepository<Customer,Integer> {
+
+    @Query("{'customerName':{$in:[?0]}}")
+    public List<Customer> getAllTheDetailsWhoHaveBoughtSamsung(String productName);
+}
