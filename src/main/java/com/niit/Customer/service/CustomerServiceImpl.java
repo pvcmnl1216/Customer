56940d60d7bd1054058e52b849fe1d656abdf9de
Philip Meshach <Philipmeshach.pm@gmail.com>
2022-12-29 16:21:43 +0530

    created the CustomerServiceImpl.java and imports a method inside it

---
+++ src/main/java/com/niit/Customer/service/CustomerServiceImpl.java
@@ -0,0 +1,32 @@
+/*
+ * Author Name: Philip Meshach
+ * Date: 29-12-2022
+ * Praise The Lord
+ */
+package com.niit.Customer.service;
+
+import com.niit.Customer.domain.Customer;
+
+import java.util.List;
+
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public Customer addCustomer(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> getAll() {
+        return null;
+    }
+
+    @Override
+    public String deleteCustomer(Integer customerId) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> getAllTheDetailsWhoHaveBoughtSamsung(String productName) {
+        return null;
+    }
+}
